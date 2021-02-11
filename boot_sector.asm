01552f142001b84cb7d2ae1fc5b47304038673dc
b3nric <15021892+b3nric@users.noreply.github.com>
2021-02-11 21:42:43 +0000

    print 'Hello, World!' to tty through interrups

--- boot_sector.asm
+++ boot_sector.asm
@@ -4,9 +4,39 @@
 ; bytes long. The last to bytes of the sector must be 0xAA55 as the
 ; BIOS checks this to make sure the boot sector is valid.
 
-; infinte loop (e9 fd ff)
-loop:
-    jmp loop
+; http://www.ctyme.com/intr/rb-0106.htm
+; the Int10/AH=0Eh interrupt sets the video ouput to tty mode and 
+; writes the character currently in AL
+
+mov ah, 0x0e ; tty mode
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, ','
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'W'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, 'r'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'd'
+int 0x10
+mov al, '!'
+int 0x10
+
+; infinte loop
+jmp $
 
 ; fill the rest of the sector with zero's
 ; $ evaluates to the assembly position at the beginning of the line
